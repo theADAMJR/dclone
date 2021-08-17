f4eac96b5481e691f8e53e541050e726122f9255
ADAMJR <adamjf123@gmail.com>
2021-08-17 21:29:41 +0100

    Initialize Git Repo

---
+++ frontend/craco.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  plugins: [
+    { plugin: require('craco-plugin-scoped-css') }
+  ]
+}
---
+++ frontend/src/components/app.tsx
@@ -0,0 +1,13 @@
+export interface AppProps {
+  
+}
+ 
+const App: React.SFC<AppProps> = () => {
+  return (
+    <div className="bg-indigo-900">
+      <h1>Hello Earth</h1>
+    </div>
+  );
+}
+ 
+export default App;
